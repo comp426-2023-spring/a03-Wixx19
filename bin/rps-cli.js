2b2e7d979dcb08e6d28bacc3f53b5e7446b0af37
William Garcia-Sanchez <89278772+Wixx19@users.noreply.github.com>
2023-04-08 22:21:29 -0400

    fixing imports

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import minimist from "minimist";
-import { playRps } from 'node-rpsls';
+import minimist from 'minimist';
+import { playRps } from '../lib/rpsls.js';
 
 const args = minimist(process.argv.slice(2));
 
